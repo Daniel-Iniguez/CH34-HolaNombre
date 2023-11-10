b9a484c262f60ef71d3505629c19db5dba71383a
Daniel Iñiguez <danielig9@outlook.com>
2023-11-10 15:06:04 -0600

    se modifico el script

--- practica.js
+++ practica.js
@@ -1,5 +1,5 @@
 function changeName(){
     let name = prompt('Cual es tu nombre?');
     const refName = document.getElementById('names');
-    refName.innerHTML = `<span class="badge bg-secondary"> Hola <strong>${name}</strong>!</span>`;
+    refName.innerHTML = `<span class="badge bg-secondary"> Hola ${name}!</span>`;
 }
